e097466e2d6d9b0c8c4bd6c6173956159b03b15e
karpovichart <karpovichart@gmail.com>
2020-09-06 21:32:53 +0300

    fix deleteStack.py

--- deleteStack.py
+++ deleteStack.py
@@ -1,7 +1,7 @@
 import subprocess
 
 stack_name = "myteststack"
-out = subprocess.check_output('s3 rm s3://testbbucket31 --recursive', shell=True).decode("utf-8")
+out = subprocess.check_output('aws s3 rm s3://testbbucket31 --recursive', shell=True).decode("utf-8")
 print(out)
 out = subprocess.check_output('aws cloudformation delete-stack --stack-name  ' + stack_name, shell=True).decode("utf-8")
 print(out)
